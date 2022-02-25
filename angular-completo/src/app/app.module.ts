3ca1b5dd2a6c1e1c24a6db6dc4d411eda6f83a47
pelocam <pelocam@gmail.com>
2022-02-25 19:03:28 -0300

    trabalhando com module

--- angular-completo/src/app/app.module.ts
+++ angular-completo/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './components/meu-primeiro/meu-primeiro.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MeuPrimeiroComponent } from './components/meu-primeiro/meu-primeiro.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
